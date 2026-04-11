ffeb6c607028904cbfead85a7dd497e4c94a4b0a
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-04-11 09:39:25 +0530

    rubyx: configs: Move `manifest_media_c2_V1_2_default.xml` to device tree
    * hmm let's try dolby vision...
    
    Change-Id: I59ceeec26346c0a04a636fa840218dd75b703b7b
    Signed-off-by: rajdeep-3305 <rajdeepbiswas3305@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,7 +88,9 @@ TARGET_SCREEN_DENSITY := 440
 BOARD_KERNEL_SEPARATED_DTBO := true
 
 # HIDL
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MANIFEST_FILE += \
+    $(DEVICE_PATH)/configs/vintf/manifest.xml \
+    $(DEVICE_PATH)/configs/vintf/manifest_media_c2_V1_2_default.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
